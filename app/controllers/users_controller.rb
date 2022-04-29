c4bd155eb6643fdf83103f5ba499d366b2dbca89
jbfermor <jbfermor@hotmail.com>
2022-04-29 11:13:01 +0200

    [user] fixed controller to permit show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[ show edit update destroy ]
-  before_action :is_admin
+  before_action :is_admin, except: :show
 
   # GET /users or /users.json
   def index
